d9a4db77ecea4dd815005a4cdea91c5cf81be439
Matthew MacLeod <mgmacleod8@gmail.com>
2023-09-03 12:18:12 -0400

    GH-9: cleanup and refactor

--- src/main/java/com/missinggreenmammals/octatrack/OctatrackExtension.java
+++ src/main/java/com/missinggreenmammals/octatrack/OctatrackExtension.java
@@ -3,11 +3,9 @@ package com.missinggreenmammals.octatrack;
 import com.bitwig.extension.controller.ControllerExtension;
 import com.bitwig.extension.controller.ControllerExtensionDefinition;
 import com.bitwig.extension.controller.api.ControllerHost;
-import com.bitwig.extension.controller.api.DocumentState;
-import com.bitwig.extension.controller.api.SettableEnumValue;
 
 public class OctatrackExtension extends ControllerExtension {
-
+	@SuppressWarnings("unused")
 	private Octatrack octatrack;
 
 	protected OctatrackExtension(ControllerExtensionDefinition definition, ControllerHost host) {
@@ -18,12 +16,8 @@ public class OctatrackExtension extends ControllerExtension {
 	public void init() {
 		final ControllerHost host = getHost();
 		host.showPopupNotification("Octatrack Initialized");
-		
-		final DocumentState documentState = host.getDocumentState();
-		final SettableEnumValue configTypeSetting = documentState.getEnumSetting("Type", "Configuration",
-				Octatrack.CONFIG_TYPE_OPTIONS, Octatrack.CONFIG_TYPE_OPTIONS[0]);
 
-		octatrack = new Octatrack(host, configTypeSetting);
+		octatrack = new Octatrack(host);
 	}
 
 	@Override
