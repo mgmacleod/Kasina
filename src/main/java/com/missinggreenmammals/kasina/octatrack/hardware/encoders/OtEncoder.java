4c4aafcb478660bb82e9cd676979cffeb6e20085
Matthew MacLeod <mgmacleod8@gmail.com>
2023-09-07 19:17:57 -0400

    GH-30: added code to represent encoders

---
+++ src/main/java/com/missinggreenmammals/kasina/octatrack/hardware/encoders/OtEncoder.java
@@ -0,0 +1,23 @@
+package com.missinggreenmammals.kasina.octatrack.hardware.encoders;
+
+import com.bitwig.extension.controller.api.AbsoluteHardwareKnob;
+import com.bitwig.extension.controller.api.ControllerHost;
+import com.bitwig.extension.controller.api.HardwareSurface;
+import com.missinggreenmammals.kasina.octatrack.hardware.OtHardwareElement;
+
+import lombok.Getter;
+
+@Getter
+public abstract class OtEncoder extends OtHardwareElement {
+
+	protected final AbsoluteHardwareKnob knob;
+
+	public OtEncoder(final int midiChannel, final int otTrack, final String name, final ControllerHost host,
+			final HardwareSurface hardwareSurface) {
+
+		super(midiChannel, otTrack);
+
+		knob = hardwareSurface.createAbsoluteHardwareKnob(createId(name));
+	}
+
+}
---
+++ src/main/java/com/missinggreenmammals/kasina/octatrack/hardware/encoders/OtPitchBendEncoder.java
@@ -0,0 +1,15 @@
+package com.missinggreenmammals.kasina.octatrack.hardware.encoders;
+
+import com.bitwig.extension.controller.api.ControllerHost;
+import com.bitwig.extension.controller.api.HardwareSurface;
+
+public class OtPitchBendEncoder extends OtEncoder {
+
+	public OtPitchBendEncoder(final int midiChannel, final int otTrack, final String name, final ControllerHost host,
+			final HardwareSurface hardwareSurface) {
+
+		super(midiChannel, otTrack, name, host, hardwareSurface);
+
+		knob.setAdjustValueMatcher(host.getMidiInPort(0).createAbsolutePitchBendValueMatcher(midiChannel));
+	}
+}
